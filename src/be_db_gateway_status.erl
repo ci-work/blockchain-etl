9b832d4b63ce4e605eeeb68fe0777aae906e05cb
ci-work <ci@work>
2022-08-15 13:57:12 +0100

    lower MAX_REQUEST_RATE

--- src/be_db_gateway_status.erl
+++ src/be_db_gateway_status.erl
@@ -36,7 +36,7 @@
 -define(STATUS_REFRESH_MINS, 10).
 %% Maximum number of status updates per second to limit the number of
 %% spawned updates.
--define(MAX_REQUEST_RATE, 500).
+-define(MAX_REQUEST_RATE, 200).
 %% A peer is recently added if it's (first) add_gateway transaction is in the
 %% last "48 hours" in blocks (60 blocks per hour assumed)
 -define(HOTSPOT_RECENTLY_ADDED_BLOCKS, 60 * 48).
